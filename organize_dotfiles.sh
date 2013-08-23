e9f03faf13779713f6a78a14104139546f7c6432
Pascal Luxain <pl@luxain.pro>
2013-08-23 17:19:25 +0200

    Add finer granularity and safer save path

--- organize_dotfiles.sh
+++ organize_dotfiles.sh
@@ -5,29 +5,31 @@
 
 # Variables
 dir=~/dotfiles
-savedir=~/dotfiles_save
+savedir=~/dotfiles_save/`date "+%s"`
 files="vimrc bash_profile gitconfig"
 folders="vim"
 
-echo "Creating save directory $savedir for backup any existing dotfiles in home"
-mkdir -p $savedir
-echo "$savedir created"
+if [ ! -d $savedir ]; then
+    echo "Creating save directory $savedir for backup any existing dotfiles in home"
+    mkdir -p $savedir
+    echo "$savedir created"
+fi
 
 # Moving files existing in $save_dir
-for file in $files; do
-	if [ -f ~/.$file ]; then
-		echo "Moving .$file in $HOME to $savedir for backup"
-		mv ~/.$file $savedir/
-	fi
-	echo "Creating symlink from $dir/$file to $HOME/.$file"
-	ln -s $dir/$file ~/.$file
-done
+#for file in $files; do
+#	if [ -f ~/.$file ]; then
+#		echo "Moving .$file in $HOME to $savedir for backup"
+#		mv ~/.$file $savedir/
+#	fi
+#	echo "Creating symlink from $dir/$file to $HOME/.$file"
+#	ln -s $dir/$file ~/.$file
+#done
 
 for folder in $folders; do
 	if [ -d ~/.$folder ]; then
 		echo "Moving .$folder folder from $HOME to $savedir for backup"
 		mv ~/.$folder $savedir/
 	fi
-	echo "Creating symlin from $dir/$folder folder to $HOME/.$folder"
+	echo "Creating symlink from $dir/$folder folder to $HOME/.$folder"
 	ln -s $dir/$folder ~/.$folder
 done
